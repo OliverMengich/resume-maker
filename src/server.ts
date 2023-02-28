ef0aa953e033cd7c01f4472e321d228377656114
OliverMengich <kemeioliver2000@gmail.com>
2023-02-28 19:03:57 +0300

    File rendering

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express,{Response} from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(expressLayouts);
 app.use(express.urlencoded({extended: true}));
-app.set('views','');
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.get('/',(req,res: Response)=>{
-    res.send('<h1>Welcome to resume maker</h1>')
+app.use(express.static(path.join(__dirname,'./public')))
+app.get('/',(_,res: Response)=>{
+    res.render('index')
 });
 export default app;
