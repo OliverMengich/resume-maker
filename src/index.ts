f366f448344b61a3892cef55fa70c3cba9cdfeef
OliverMengich <kemeioliver2000@gmail.com>
2023-03-05 07:54:04 +0300

    Styles UI

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import server from './server';
-server.listen(3000,()=>{
-    console.log("Server running on port 3000");
+server.listen(4000,()=>{
+    console.log("Server running on port http://localhost:4000");
 });
