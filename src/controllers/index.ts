6b567743131e7fc12abca475d44b88c9bb618330
OliverMengich <kemeioliver2000@gmail.com>
2023-03-07 23:05:41 +0300

    Render controllers content to frontend

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -18,14 +18,20 @@ type Contact = {
     linkedin: string,
     github: string,
     location: string,
-    twitter: string
+    twitter: string,
+    phone: string
+}
+type User = {
+    name: string
+    role: string
 }
 type Data = {
     experience: Description[],
     education: Description[],
     skills: string[],
     contact: Contact,
-    projects: Description[]
+    projects: Description[],
+    description: User
 }
 type Controller = {
     data: Data,
@@ -35,18 +41,24 @@ type Controller = {
 
 const controller: Controller = {
     data: {
+        description:{
+            name: 'Oliver Kipkemei',
+            role: 'Software Developer',
+        },
         experience: [
             {
                 name: 'Cognizant',
                 Title: 'Software Engineer',
                 description: 'Worked on a project for a client in the healthcare industry. The project was to build a web application for the client to manage their patients and their medical records. The application was built using React, Node.js, Express, and MongoDB. I was responsible for building the backend of the application using Node.js and Express. I also worked on the frontend of the application using React.',
-                start_date: 'June 2020',
+                start_date: '06/2020',
                 end_date: 'Present'
             },
             {
                 name: 'Cognizant',
                 Title: 'Software Engineer Intern',
                 description: 'Worked on a project for a client in the healthcare industry. The project was to build a web application for the client to manage their patients and their medical records. The application was built using React, Node.js, Express, and MongoDB. I was responsible for building the backend of the application using Node.js and Express. I also worked on the frontend of the application using React.',
+                start_date: '05/2019',
+                end_date: '04/2020'
             }
         ],
         education: [
@@ -99,11 +111,12 @@ const controller: Controller = {
             linkedin: 'https://www.linkedin.com/in/oliver-arsenal',
             github: 'https://github.com/OliverMengich',
             location: 'Nairobi, Kenya',
-            twitter: 'https://www.twitter.com/OllieKem7'
+            twitter: 'https://www.twitter.com/OllieKem7',
+            phone: '+254741954425'
         }
     },  
     get: function(){
-        return this.data 
+        return this.data;
     },
     set: function(category: keyof Data, update: {[key: string ]: string} | string[]){
         const categoryData = this.data[category];
@@ -114,3 +127,4 @@ const controller: Controller = {
         }
     }
 }
+export default controller;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express,{Response} from 'express';
 import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
+import controller from './controllers';
 const app = express();
 app.use(express.json());
 app.use(expressLayouts);
@@ -9,6 +10,8 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'./public')))
 app.get('/',(_,res: Response)=>{
-    res.render('index')
+    res.render('index',{
+        info: controller.data
+    })
 });
 export default app;
