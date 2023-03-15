3b41d060ccba136f627c4e5a877a79bfd11eb690
OliverMengich <kemeioliver2000@gmail.com>
2023-03-15 08:26:10 +0300

    Now includes views and public folder in deployment

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(expressLayouts);
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'./public')))
+console.log(path.join(__dirname,'public'))
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname,'./public')))
 app.get('/',(_,res: Response)=>{
     res.render('index',{
         info: controller.data
