42acb3b2ab2602b659399d983e7e0a2005df8180
OliverMengich <kemeioliver2000@gmail.com>
2023-03-17 12:51:47 +0300

    Adds print button

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,29 @@
-import express,{Response} from 'express';
+import express,{Request, Response} from 'express';
+import jspdf from 'jspdf'
+
 import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
 import controller from './controllers';
 const app = express();
 app.use(express.json());
+app.use(express.raw())
 app.use(expressLayouts);
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'./public')))
 console.log(path.join(__dirname,'public'))
-app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static("C:/Users/olive/Documents/VSCODE_PROJECTS/resume-maker/node_modules/.pnpm/html2canvas@1.4.1/node_modules/html2canvas/dist/types/index"))
 app.get('/',(_,res: Response)=>{
     res.render('index',{
         info: controller.data
     })
 });
+app.post('/print-pdf',(req: Request,res:Response)=>{
+    console.log('I was called');
+    console.log(req.body);
+    
+    return res.status(200).json({message: 'successful'})
+})
 export default app;
